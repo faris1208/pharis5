7ff76aadde2a392bdf24b5cdc7e0640ea0617113
faris1208 <139537951+faris1208@users.noreply.github.com>
2023-08-29 10:54:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let submit = document.getElementById('submit');
+let rectangle = document.getElementById('rectangle');
+
+// document.getElementById("button").innerHTML = rectangle;
+// const colors=['blue','green'];
+
+let changerectanglecolor = function () {
+    rectangle.style.background = 'blue';
+};
+
+
+submit.addEventListener('click',changerectanglecolor);
+
+
+
+// const points = new fruits[];
+// document.getElementById("h1").innerHTML = points
+
+// let mytext = "this is my twextv"
+// letmynuwe = 7787
